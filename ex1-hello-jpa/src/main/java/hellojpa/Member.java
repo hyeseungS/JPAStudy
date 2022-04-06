cd42303c4a6821b22d887b04f29a33a5f8171890
hyeseungS <hyeseung2000@gmail.com>
2022-04-06 13:26:15 +0900

    Add : ch04 - JPA 시작에 매핑 추가

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,18 +1,35 @@
 package hellojpa;
 
 import javax.persistence.*;  //**
+import java.util.Date;
 
 @Entity
+@Table(name = "MEMBER", uniqueConstraints = {@UniqueConstraint( // 추가
+        name = "NAME_AGE_UNIQUE",
+        columnNames = {"NAME", "AGE"} )})
 public class Member {
 
     @Id
-    @Column(name = "ID")
+    @GeneratedValue
     private Long id;
 
-    @Column(name = "NAME")
+    @Column(name = "NAME", nullable = false, length = 10) // 추가
     private String username;
 
     private Integer age;
+
+    // == 추가 == //
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
 
     public Long getId() {
         return id;
@@ -37,4 +54,37 @@ public class Member {
     public void setAge(Integer age) {
         this.age = age;
     }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
